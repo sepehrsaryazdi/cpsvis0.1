b17ddd45d41624eacc40a0f78f4ae80009ce1547
sepehrsaryazdi <sepehr.saryazdi@gmail.com>
2022-01-18 21:11:40 +1100

    Added decorated_triangles file

---
+++ decorated_triangles/triangle.py
@@ -0,0 +1,14 @@
+
+class Decoration:
+    def __init__(self):
+        pass
+
+class Triangle:
+    def __init__(self):
+        pass
+    def update_decoration(self):
+        pass
+
+class Surface:
+    def __init__(self):
+        pass
---
+++ test.py
@@ -0,0 +1,22 @@
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = Axes3D(fig, auto_add_to_figure=False)
+fig.add_axes(ax)
+x1 = [1,0,1]
+y1 = [0,1,1]
+z1 = [0,0,1]
+x2 = [1,1,4]
+y2 = [0,1,1]
+z2 = [0,1,2]
+verts = [list(zip(x1,y1,z1)),list(zip(x2,y2,z2))]
+colors = ["tab:blue" if np.random.rand()<0.1 else "tab:orange" for vert in verts]  # MWE colors
+patchcollection = Poly3DCollection(verts,linewidth=1,edgecolor="k",facecolor = colors,rasterized=True)
+ax.add_collection3d(patchcollection)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+#ax.add_collection3d(Poly3DCollection(verts))
+plt.show()
