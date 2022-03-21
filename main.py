d635dd5542f99498a2fe01c4e28c8a95bd56a668
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2022-03-21 20:43:28 +1100

    Fixed gluing table examples

--- main.py
+++ main.py
@@ -1901,9 +1901,6 @@ class CombinatorialImport:
             for row in gluing_table_array:
                 for param in row:
                     assert param > 0
-            gluing_table_array_last_col = gluing_table_array[:,-1].copy()
-            gluing_table_array[:,-1] = gluing_table_array[:,-2]
-            gluing_table_array[:,-2] = gluing_table_array_last_col
             self.input_parameters = gluing_table_array
         edges = columns[1:4]
         gluing_table = np.array(gluing_table.values.tolist())
@@ -2689,24 +2686,23 @@ def import_file():
     filename = filedialog.askopenfilename(filetypes=[("Excel files", ".csv")])
     if not filename:
         return
-    combinatorial_plot_window = CombinatorialImport(tk, filename)
-    # try:
-    #     gluing_table = pd.read_table(filename)
-    #     columns = gluing_table.columns
-    #     for row in np.array(gluing_table):
-    #         for element in row[0].rsplit(','):
-    #             assert element
-    #     combinatorial_plot_window = CombinatorialImport(tk, filename)
-    # except:
-    #     win = tk.Toplevel()
-    #     win.wm_title("Gluing Table Invalid")
-    #     l = tk.Label(win, text="There was an error uploading this gluing table. Please ensure you have a valid gluing table before continuing.")
-    #     l.pack(side="top",padx=20, pady=10)
-    #     l2 = tk.Label(win, text="Explicit examples of the required structure are available in the 'example_gluing_tables' folder. Note that every edge on all triangles must be glued.")
-    #     l2.pack(side="top",padx=20,pady=(0,10))
-    #     #win.iconphoto(False, tk.PhotoImage(file='./misc/Calabi-Yau.png'))
-    #     cancel = ttk.Button(win, text="Close", command=win.destroy)
-    #     cancel.pack(side='right', padx=25, pady=5)
+    try:
+        gluing_table = pd.read_table(filename)
+        columns = gluing_table.columns
+        for row in np.array(gluing_table):
+            for element in row[0].rsplit(','):
+                assert element
+        combinatorial_plot_window = CombinatorialImport(tk, filename)
+    except:
+        win = tk.Toplevel()
+        win.wm_title("Gluing Table Invalid")
+        l = tk.Label(win, text="There was an error uploading this gluing table. Please ensure you have a valid gluing table before continuing.")
+        l.pack(side="top",padx=20, pady=10)
+        l2 = tk.Label(win, text="Explicit examples of the required structure are available in the 'example_gluing_tables' folder. Note that every edge on all triangles must be glued.")
+        l2.pack(side="top",padx=20,pady=(0,10))
+        #win.iconphoto(False, tk.PhotoImage(file='./misc/Calabi-Yau.png'))
+        cancel = ttk.Button(win, text="Close", command=win.destroy)
+        cancel.pack(side='right', padx=25, pady=5)
 
 
 def convert_surface_to_gluing_table(self):
@@ -2748,10 +2744,6 @@ def export_file():
         gluing_table_data.append(row)
     
     gluing_table_data = np.array(gluing_table_data)
-    gluing_table_data_last_col = gluing_table_data[:,-1].copy()
-    gluing_table_data[:,-1] = gluing_table_data[:,-2]
-    gluing_table_data[:,-2] = gluing_table_data_last_col
-
     parameter_table_data = []
 
     for triangle in app.abstract_surface.triangles:
@@ -2763,11 +2755,7 @@ def export_file():
         parameter_table_data.append(row)
     
     parameter_table_data = np.array(parameter_table_data)
-    parameter_table_data_last_col = parameter_table_data[:,-1].copy()
-    parameter_table_data[:,-1] = parameter_table_data[:,-2]
-    parameter_table_data[:,-2] = parameter_table_data_last_col
-
-    column_names = np.array(['Triangle', 'Edge 01', 'Edge 02', 'Edge 12', 'Triangle Parameter', 'Edge 01', 'Edge 02', 'Edge 12'])
+    column_names = np.array(['Triangle', 'Edge 01', 'Edge 12', 'Edge 20', 'Triangle Parameter', 'Edge 01', 'Edge 12', 'Edge 20'])
     data = np.hstack([gluing_table_data,parameter_table_data])
     table = pd.DataFrame(data)
     table.columns = column_names
