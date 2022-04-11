Fixed precision error

helper_functions/length_heat_map.py
@@ -6,7 +6,7 @@ from matplotlib.ticker import MaxNLocator
 import matplotlib.cm as cm
 from helper_functions.add_new_triangle_functions import *
 import mpmath as mp
-mp.mp.dps = 50
+mp.mp.dps = 100
 mp.mp.pretty = False
 
 
@@ -33,8 +33,8 @@ class LengthHeatMapTree:
         if depth:
             self.create_nodes(initial_node)
         
-        print(self.smallest_length)
-        print([n.index for n in self.smallest_nodes])
+        #print(self.smallest_length)
+        #print([n.index for n in self.smallest_nodes])
     
     def move_to_vector(self, move):
         return np.array({'R':[0,1],'U':[1,0], 'L': [0,-1], 'D': [-1,0]}[move])
@@ -55,10 +55,10 @@ class LengthHeatMapTree:
                 next_node.matrix = starting_node.matrix*self.move_to_matrix(allowed_moves[move_index])
                 next_node.length, _ =get_length(next_node.matrix)
                 next_node.length =  np.float16(next_node.length)
-                if round(next_node.length,5) > 0 and self.smallest_length > next_node.length + self.difference_precision:
+                if round(next_node.length,3) > 0 and self.smallest_length > next_node.length + self.difference_precision:
                     self.smallest_length = min(next_node.length,self.smallest_length)
                     self.smallest_nodes = [next_node]
-                elif round(next_node.length,5) > 0 and abs(self.smallest_length - next_node.length) < self.difference_precision:
+                elif round(next_node.length,3) > 0 and abs(self.smallest_length - next_node.length) < self.difference_precision:
                     self.smallest_length = min(self.smallest_length, next_node.length)
                     self.smallest_nodes.append(next_node)
                 self.nodes.append(next_node)

main.py
@@ -9,7 +9,7 @@ from numpy import arctan2
 from sklearn import neighbors
 from sympy import poly
 import mpmath as mp
-mp.mp.dps = 50
+mp.mp.dps = 100
 mp.mp.pretty = False
 
 
@@ -639,7 +639,7 @@ class TranslationLength:
 
             product = first_matrix*final_edge_matrix*second_matrix 
             self.representations.append(product)
-            print(product)
+            #print(product)
 
             #print(np.linalg.det(product))
             #print(product)